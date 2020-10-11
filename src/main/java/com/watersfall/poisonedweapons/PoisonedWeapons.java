d749a0e01cc947eb354680e39a30487d8de0a6f7
Watersfall <dontshoot262@gmail.com>
2020-10-11 15:44:07 -0400

    Move affect applying to event

--- src/main/java/com/watersfall/poisonedweapons/PoisonedWeapons.java
+++ src/main/java/com/watersfall/poisonedweapons/PoisonedWeapons.java
@@ -1,12 +1,14 @@
 package com.watersfall.poisonedweapons;
 
+import com.watersfall.poisonedweapons.event.ApplyAffectEvent;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.player.AttackEntityCallback;
 
 public class PoisonedWeapons implements ModInitializer
 {
 	@Override
 	public void onInitialize()
 	{
-
+		AttackEntityCallback.EVENT.register(new ApplyAffectEvent());
 	}
 }
