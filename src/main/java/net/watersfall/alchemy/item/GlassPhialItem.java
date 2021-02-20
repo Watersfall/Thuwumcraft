2965263a5a8071ffa6726ee9b96b273bf48d9b20
Watersfall <dontshoot262@yahoo.com>
2021-02-20 09:12:24 -0500

    Add missing localization

--- src/main/java/net/watersfall/alchemy/item/GlassPhialItem.java
+++ src/main/java/net/watersfall/alchemy/item/GlassPhialItem.java
@@ -3,9 +3,13 @@ package net.watersfall.alchemy.item;
 import net.fabricmc.fabric.api.block.entity.BlockEntityClientSerializable;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
+import net.minecraft.text.LiteralText;
+import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
@@ -14,6 +18,9 @@ import net.watersfall.alchemy.api.aspect.Aspect;
 import net.watersfall.alchemy.api.aspect.AspectInventory;
 import net.watersfall.alchemy.api.aspect.AspectStack;
 import net.watersfall.alchemy.api.aspect.Aspects;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class GlassPhialItem extends Item
 {
@@ -121,5 +128,28 @@ public class GlassPhialItem extends Item
 		}
 		stack.getOrCreateTag().putInt("waters_aspect_count", count);
 		return count > 0;
+	}
+
+	@Override
+	public String getTranslationKey()
+	{
+		if(this.aspect == Aspect.EMPTY)
+		{
+			return "item.waters_alchemy_mod.phial.empty";
+		}
+		else
+		{
+			return "item.waters_alchemy_mod.phial";
+		}
+	}
+
+	@Override
+	public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context)
+	{
+		super.appendTooltip(stack, world, tooltip, context);
+		if(this.aspect != Aspect.EMPTY)
+		{
+			tooltip.add(new LiteralText(this.getAspectCount(stack) + " ").append(new TranslatableText(this.aspect.getTranslationKey())));
+		}
 	}
 }
