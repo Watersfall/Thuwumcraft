a4e2643bb5fb0bfda9acbbeb7fe712c30047076d
Watersfall <dontshoot262@yahoo.com>
2021-03-04 01:39:04 -0500

    Remove unnecessary accessor from PlayerAdvancementTrackerMixin

--- src/main/java/net/watersfall/alchemy/accessor/waters_PlayerAdvancementTrackerAccessor.java
+++
@@ -1,8 +0,0 @@
-package net.watersfall.alchemy.accessor;
-
-import net.minecraft.server.network.ServerPlayerEntity;
-
-public interface waters_PlayerAdvancementTrackerAccessor
-{
-	ServerPlayerEntity getOwner();
-}
--- src/main/java/net/watersfall/alchemy/mixin/PlayerAdvancementTrackerMixin.java
+++ src/main/java/net/watersfall/alchemy/mixin/PlayerAdvancementTrackerMixin.java
@@ -4,7 +4,6 @@ import net.minecraft.advancement.Advancement;
 import net.minecraft.advancement.PlayerAdvancementTracker;
 import net.minecraft.entity.Entity;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.watersfall.alchemy.accessor.waters_PlayerAdvancementTrackerAccessor;
 import net.watersfall.alchemy.api.abilities.AbilityProvider;
 import net.watersfall.alchemy.api.abilities.entity.PlayerResearchAbility;
 import org.spongepowered.asm.mixin.Mixin;
@@ -14,20 +13,14 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(PlayerAdvancementTracker.class)
-public class PlayerAdvancementTrackerMixin implements waters_PlayerAdvancementTrackerAccessor
+public class PlayerAdvancementTrackerMixin
 {
 	@Shadow private ServerPlayerEntity owner;
-
-	@Override
-	public ServerPlayerEntity getOwner()
-	{
-		return this.owner;
-	}
 
 	@Inject(method = "grantCriterion", at = @At(value = "INVOKE", target = "Lnet/minecraft/advancement/AdvancementRewards;apply(Lnet/minecraft/server/network/ServerPlayerEntity;)V"))
 	public void checkIsDone(Advancement advancement, String criterionName, CallbackInfoReturnable<Boolean> cir)
 	{
-		AbilityProvider<Entity> provider = AbilityProvider.getProvider(this.getOwner());
+		AbilityProvider<Entity> provider = AbilityProvider.getProvider(this.owner);
 		provider.getAbility(PlayerResearchAbility.ID, PlayerResearchAbility.class).ifPresent((ability -> {
 			ability.addAdvancement(advancement.getId());
 		}));
