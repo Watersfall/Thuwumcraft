ee19d0f3b784b439cb149819f1b77cf758fd4628
Watersfall <dontshoot262@gmail.com>
2021-06-29 15:25:26 -0400

    Fix recipes failing to load on dedicated servers

--- src/main/java/net/watersfall/thuwumcraft/recipe/ResearchUnlockedRecipe.java
+++ src/main/java/net/watersfall/thuwumcraft/recipe/ResearchUnlockedRecipe.java
@@ -105,7 +105,15 @@ public class ResearchUnlockedRecipe<T extends CraftingRecipe> implements Researc
 	@Override
 	public RecipeSerializer<?> getSerializer()
 	{
-		return ThuwumcraftRecipes.RESEARCH_UNLOCKED_SHAPED_RECIPE_SERIALIZER;
+		//TODO: something about this
+		if(recipe instanceof ShapedRecipe)
+		{
+			return ThuwumcraftRecipes.RESEARCH_UNLOCKED_SHAPED_RECIPE_SERIALIZER;
+		}
+		else
+		{
+			return ThuwumcraftRecipes.RESEARCH_UNLOCKED_SHAPELESS_RECIPE_SERIALIZER;
+		}
 	}
 
 	@Override
